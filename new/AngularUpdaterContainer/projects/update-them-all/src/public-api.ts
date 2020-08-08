0916c4b10ac7e22a698ece711556bd32e120513e
Gerrit Verse <gerritverse@gmail.com>
2020-08-08 10:59:03 +0200

    neue Struktur verworfen

--- new/AngularUpdaterContainer/projects/update-them-all/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of update-them-all
- */
-
-export * from './lib/update-them-all.service';
-export * from './lib/update-them-all.component';
-export * from './lib/update-them-all.module';
