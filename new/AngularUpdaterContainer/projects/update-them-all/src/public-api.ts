40138509261789cbb8a2e395cdac64346bbf6c4d
Gerrit Verse <gerritverse@gmail.com>
2020-08-08 10:47:46 +0200

    init projekt + lib

---
+++ new/AngularUpdaterContainer/projects/update-them-all/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of update-them-all
+ */
+
+export * from './lib/update-them-all.service';
+export * from './lib/update-them-all.component';
+export * from './lib/update-them-all.module';
