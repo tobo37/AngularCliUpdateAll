8a7541909f38a349d22298ccb6c0cdcc03403223
Gerrit Verse <gerritverse@gmail.com>
2020-10-13 21:27:38 +0200

    npm audit fix

--- src/cli.ts
+++ src/cli.ts
@@ -51,7 +51,7 @@ async function promptForMissingOptions(options: UpdateOptions) {
         default: 'all',
     });
 
-    const answers: 'dependencies' | 'devDependencies' | 'all'  = await inquirer.prompt(questions);
+    const answers: 'dependencies' | 'devDependencies' | 'all'  = (await inquirer.prompt(questions)).toString();
     console.log(chalk.green("choosed: " +answers));
     options[answers] = true;
     return options;
