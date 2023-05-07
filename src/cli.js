e7fc3f7cd911d08e6152eaaa1767105eefdf57d6
Gerrit Verse <gerritverse@gmail.com>
2023-05-07 13:49:09 +0200

    bla

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,4 @@
 const { updateAll } = require("./update-packages")
 const [,, ...args] = process.argv
 
-updateAll().then(() => console.log("update complete"))
+updateAll().then(() => console.log("update complete")).catch((error) => console.error(error))
